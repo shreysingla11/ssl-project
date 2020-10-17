c4480aba9402a1cf4315d38335b9367d2c1637db
Shrey <shreysingla@cse.iitb.ac.in>
2020-10-17 22:41:04 +0530

    Added whole parser functioning

--- parser/parser.py
+++ parser/parser.py
@@ -1,33 +1,53 @@
 import numpy as np
 import re
+from collections import defaultdict
 
-f = open("1.cpp","r")
-w = open("temp","w+")
-
-for line in f.readlines():
-	l=line.split("//",1)
-	w.write(l[0])
-w.close()
-w = open("temp","r")
-
-special_char=['+','-','=','*','/','[',']','{','}']
-
-f_com = re.sub('\n','\t',w.read())
-f_com = re.sub(r'(\/\*).+?(\*\/)','',f_com)
-w.close()
+f = open("2.cpp","r")
 w = open("temp","w")
 
-w.write(f_com)
+pat=re.compile(r'(\/\*).+?(\*\/)',re.DOTALL)
+s = re.sub(pat,'',f.read())
+
+s1=""
+for line in s.split("\n"):
+	l=line.split("//",1)
+	s1=s1+l[0]+"\n"
 
 
-"""
-while(1):
-	char=f.read(1)
+special_char=['+','-','=','*','/','[',']','{','}','<','>','(',')',';','#',',',':']
+keywords=open("./ckeywords.txt").read().split()
+#digits = ['0','1','2','3','4','5','6','7','8','9']
 
-	if not char:
-		break
-	if char=="\t":
-		print("Yayy")
-		continue
-	w.write(char)
-"""
+s2=""
+for char in list(s1):
+	
+	if char in special_char:
+		e=" "+char+" "
+		s2+=e
+	else:
+		s2+=char
+	"""
+	x=ord(char)
+	if x>=65 and x<=90:
+		s2+=char
+	elif x>=97 and x<=122:
+		s2+=char
+	else:
+		e=" "+char+" "
+		s2+=e
+    """
+
+text = open("temp","r") 
+myword="r"
+sf=""
+for line in s2.split("\n"):
+	#print(line)
+	for word in line.split():
+		if word not in keywords and word not in special_char:
+			sf+="r "
+		else:
+			sf+=(word+" ")
+	sf+="\n"
+
+w.write(sf)
+
