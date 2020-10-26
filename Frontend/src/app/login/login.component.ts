a655a80b2270f3b44f0534be25987eb1696dcfa2
Shrey <shreysingla@cse.iitb.ac.in>
2020-10-26 14:25:07 +0530

    typescript for forms

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form;
+
+  constructor(
+    private auth: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.form = this.formBuilder.group({
+      username: '',
+      password: '',
+      org_pass: ''
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.auth.postLogin(JSON.stringify(this.form.value)).subscribe(
+      (data) => {
+        if (data.hasOwnProperty('username')) {
+          this.router.navigate(['/dashboard']);
+        }
+        else {
+          alert("Please try again");
+        }
+
+      },
+      (error) => {
+        alert("Please try again");
+      }
+    )
   }
 
 }
--- Frontend/src/app/register/register.component.ts
+++ Frontend/src/app/register/register.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
+
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form;
+
+  constructor(
+    private auth: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { 
+    this.form = this.formBuilder.group({
+      username: '',
+      email: '',
+      first_name: '',
+      last_name: '',
+      password: ''
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.auth.postRegister(JSON.stringify(this.form.value)).subscribe(
+      (data) => {
+        if (data.hasOwnProperty('username')){
+          this.router.navigate(['/login']);
+        }
+        else {
+          alert("Please try again");
+        }
+        
+      },
+      (error) => {
+        alert("Please try again");
+      }
+    )
   }
 
 }
