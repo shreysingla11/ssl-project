a21fb8b7c2d842a9b959bdf8e4c337e46f7eb72b
Jayesh Singla <jayesh11.3.01@gmail.com>
2020-10-27 18:38:21 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-#!/bin/bash
+python3 Backend/manage.py makemigrations
+python3 Backend/manage.py migrate
 cd Frontend && ng serve & pids=$!
 cd Backend && python3 manage.py runserver & pids+=" $!"
 
